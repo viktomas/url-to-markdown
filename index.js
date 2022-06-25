e421d74108411ebfceb4cc7183ccd1fff0681420
Tomas Vik <tvik@gitlab.com>
2022-06-25 11:50:15 +0200

    working script

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const { Readability } = require('@mozilla/readability');
-const { JSDOM } = require('jsdom');
-const TurndownService = require('turndown');
+import { Readability } from '@mozilla/readability';
+import { JSDOM } from 'jsdom';
+import TurndownService from 'turndown';
+import fetch from 'node-fetch';
 
-const testPage = require('./test-page').content;
+if (process.argv.length !== 3) {
+  console.error('You need to pass URL as the only argument');
+  process.exit(1);
+}
+const url = process.argv[2];
 
-const doc = new JSDOM(testPage, {
-  url: 'https://blog.viktomas.com/posts/mermaidjs-diagrams-in-logseq/',
-});
-const reader = new Readability(doc.window.document);
-const article = reader.parse();
-const html = article.content;
-const turndownService = new TurndownService();
-const markdown = turndownService.turndown(html);
-console.log(markdown);
+const makeMarkdownFromUrl = async (url) => {
+  const downloadedPage = await (await fetch(url)).text();
+  const doc = new JSDOM(downloadedPage, {
+    url,
+  });
+  const reader = new Readability(doc.window.document);
+  const article = reader.parse();
+  const html = article.content;
+  const turndownService = new TurndownService({
+    headingStyle: 'atx',
+  });
+  return turndownService.turndown(html);
+};
+makeMarkdownFromUrl(url).then(console.log);
